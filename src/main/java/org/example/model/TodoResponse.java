3312c9b6e608f68377846cf15e91048b87f4bd6b
Chanyoung Kim <whqnd1106@gmail.com>
2022-01-16 22:09:54 +0900

    todo-server upload

---
+++ src/main/java/org/example/model/TodoResponse.java
@@ -0,0 +1,25 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoResponse {
+    private Long id;
+    private String title;
+    private Long order;
+    private Boolean completed;
+    private String url;
+
+    public TodoResponse(TodoModel todoModel) {
+        this.id = todoModel.getId();
+        this.title = todoModel.getTitle();
+        this.order = todoModel.getOrder();
+        this.completed = todoModel.getCompleted();
+
+        this.url = "https://localhost:8080/" + this.id;
+    }
+}
